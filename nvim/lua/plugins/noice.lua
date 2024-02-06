8d85372749fd5eb7c2761a9cb7afa5d39b2d60ea
Paolo <paolo.fabbri@boxt.co.uk>
2024-02-06 16:34:54 +0000

    update noice config to fix macro replay

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/noice.nvim",
+  config = function()
+    require("lualine").setup({
+      sections = {
+        lualine_x = {
+          {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            color = { fg = "#ff9e64" },
+          },
+        },
+      },
+    })
+  end,
+}
