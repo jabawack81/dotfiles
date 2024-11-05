f76e463684b6416ba90ec5a6b502dcffcb9f56a9
Paolo <paolo.fabbri@boxt.co.uk>
2024-11-05 17:11:31 +0000

    update telescope plugin in neovim

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -13,40 +13,40 @@ return {
     -- change some options
     opts = {
       defaults = {
-        --layout_strategy = "horizontal",
-        layout_config = {
+        layout_strategy = "horizontal",
+        -- layout_config = {
+        --
+        --   width = function(_, cols, _)
+        --     if cols > 200 then
+        --       return 170
+        --     else
+        --       return math.floor(cols * 0.9)
+        --     end
+        --   end,
+        --   height = function(_, _, lines)
+        --     if lines > 50 then
+        --       return math.floor(lines * 0.5)
+        --     else
+        --       return math.floor(lines * 0.9)
+        --     end
+        --   end,
 
-          width = function(_, cols, _)
-            if cols > 200 then
-              return 170
-            else
-              return math.floor(cols * 0.9)
-            end
-          end,
-          height = function(_, _, lines)
-            if lines > 50 then
-              return math.floor(lines * 0.5)
-            else
-              return math.floor(lines * 0.9)
-            end
-          end,
-
-          -- preview_cutoff = 120,
-          --
-          horizontal = {
-            prompt_position = "top",
-            preview_width = 0.2,
-            results_width = 0.8,
-          },
-          vertical = {
-            prompt_position = "top",
-            preview_width = 0.9,
-            results_width = 0.1,
-            --   mirror = false,
-          },
-          -- width = 0.9,
-          -- height = 0.9,
-        },
+        -- preview_cutoff = 120,
+        --
+        -- horizontal = {
+        --   prompt_position = "top",
+        --   preview_width = 0.2,
+        --   results_width = 0.8,
+        -- },
+        -- vertical = {
+        --   prompt_position = "top",
+        --   preview_width = 0.9,
+        --   results_width = 0.1,
+        --   --   mirror = false,
+        -- },
+        -- width = 0.9,
+        -- height = 0.9,
+        -- },
         --  layout_config = {
         --    width = 0.87,
         --    height = 0.80,
