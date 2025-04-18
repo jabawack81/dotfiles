12c98eade90eef6c240c779d3e9ce426765743d5
jabawack81 <jabawack81@gmail.com>
2025-04-18 20:14:30 +0100

    add script to install oh my zsh plugins

---
+++ oh-my-zsh-setup.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/paolo/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions /home/paolo/.oh-my-zsh/custom/plugins/zsh-autosuggestions
