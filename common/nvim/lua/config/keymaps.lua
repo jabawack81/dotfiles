9228e97c450d5c082003c307f4336ba0d8ee3840
Paolo <jabawack81@gmail.com>
2025-08-12 11:21:42 +0100

    add keybindings for clippboard registers

--- common/nvim/lua/config/keymaps.lua
+++ common/nvim/lua/config/keymaps.lua
@@ -4,3 +4,9 @@
 
 -- CreatePR keymap - opens GitHub PR creation page
 vim.keymap.set("n", "<leader>gp", "<cmd>CreatePR<cr>", { desc = "Create GitHub PR" })
+
+-- Register viewing keymaps (using <leader>r for registers)
+vim.keymap.set("n", "<leader>rr", "<cmd>reg<cr>", { desc = "Show all registers" })
+vim.keymap.set("n", "<leader>rc", "<cmd>reg \"<cr>", { desc = "Show copy register" })
+vim.keymap.set("n", "<leader>r+", "<cmd>reg +<cr>", { desc = "Show system clipboard" })
+vim.keymap.set("n", "<leader>ry", "<cmd>reg 0<cr>", { desc = "Show yank register" })
